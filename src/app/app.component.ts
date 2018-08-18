c4f90be22c06820ef8beca441c86bf36db3c2541
Curtis Jenkins <>
2018-08-18 19:19:46 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contestant } from './contestant';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GrilloffService } from './grilloff.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'grilloff-rating';
+  email: string;
+  contestants: Contestant[];
+  results = [];
+
+  constructor(private grillOffService: GrilloffService) { }
+
+  ngOnInit() {
+    this.getContestants();
+    // console.log(this.contestants);
+  }
+  getContestants() {
+    this.grillOffService.getContestants()
+      .subscribe( contestants => this.contestants = contestants);
+  }
+
+  rateChange(tab: string, contestant: Contestant, rate: number) {
+    const key = `${tab}:${contestant.id}`;
+    this.results[key] = {contestant: contestant, rate: rate};
+    console.log(this.results);
+  }
 }
