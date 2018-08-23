04ea496a54d870225872b8a112bf9868965bf77f
Curtis Jenkins <>
2018-08-22 22:02:45 -0400

    working on nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule, Routes } from '@angular/router';
+// import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { JudgeComponent } from './judge/judge.component';
+
+export const appRoutes: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+      path: 'login',
+      component: LoginComponent
+  },
+  {
+      path: 'judge',
+      component: JudgeComponent
+  },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    JudgeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    // AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
