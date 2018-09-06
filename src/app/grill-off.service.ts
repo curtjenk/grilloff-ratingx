1bf6ca7885e45f7ffb20a9b9ff6eb4d7b90a8a3c
curtjenkcfa <curtjenkcfa@gmail.com>
2018-09-05 20:13:26 -0400

    some cleanup and css tweaks

--- src/app/grill-off.service.ts
+++ src/app/grill-off.service.ts
@@ -30,31 +30,31 @@ export class GrillOffService {
   public readonly currentUser: Observable<Person> = this._behaviorSubject.asObservable();
   public readonly currentUserValue = () => this._behaviorSubject.getValue();
 
-  // private contestants = [
-  //   {id: 1, name: 'Anthony Chinn',   email: '', type: 1, token: ''},
-  //   {id: 2, name: 'Darnell Tolbert', email: '', type: 1, token: ''},
-  //   {id: 3, name: 'George Anderson', email: '', type: 1, token: ''},
-  //   {id: 4, name: 'Victory Speight', email: '', type: 1, token: ''},
-  //   {id: 5, name: 'Rodney Houston',  email: '', type: 1, token: ''},
-  // ];
-  // private judges = [
-  //   {id: 11, name: 'Judge 1', email: '', type: 2, token: ''},
-  //   {id: 12, name: 'Judge 1', email: '', type: 2, token: ''},
-  //   {id: 13, name: 'Judge 1', email: '', type: 2, token: ''},
-  //   {id: 14, name: 'Judge 1', email: '', type: 2, token: ''},
-  //   {id: 15, name: 'Judge 1', email: '', type: 2, token: ''},
-  // ];
+  private contestants = [
+    {id: 1, name: 'Anthony Chinn',   email: '', type: 1, token: ''},
+    {id: 2, name: 'Darnell Tolbert', email: '', type: 1, token: ''},
+    {id: 3, name: 'George Anderson', email: '', type: 1, token: ''},
+    {id: 4, name: 'Victory Speight', email: '', type: 1, token: ''},
+    {id: 5, name: 'Rodney Houston',  email: '', type: 1, token: ''},
+  ];
+  private judges = [
+    {id: 11, name: 'Judge 1', email: '', type: 2, token: ''},
+    {id: 12, name: 'Judge 2', email: '', type: 2, token: ''},
+    {id: 13, name: 'Judge 3', email: '', type: 2, token: ''},
+    {id: 14, name: 'Judge 4', email: '', type: 2, token: ''},
+    {id: 15, name: 'Judge 5', email: '', type: 2, token: ''},
+  ];
 
   constructor(private http: HttpClient) { }
 
   getContestants(): Observable<Person[]> {
     // console.log('service:getContestants');
-    // return of(this.contestants);
-    const url = host + '/' + baseUrl.contestants;
-    return this.http.get<Person[]>(url, httpOptions)
-      .pipe(
-        catchError(this.handleError('getContestants', []))
-      );
+    return of(this.contestants);
+    // const url = host + '/' + baseUrl.contestants;
+    // return this.http.get<Person[]>(url, httpOptions)
+    //   .pipe(
+    //     catchError(this.handleError('getContestants', []))
+    //   );
   }
   saveContestant(newPerson: Person): Observable<Person> {
     newPerson.id = null;
@@ -134,12 +134,13 @@ export class GrillOffService {
     };
     const url = host + '/' + baseUrl.user;
     const options = Object.assign({params: {name: person.name, email: person.email}, httpOptions});
-
-    return this.http.get<Person>(url, options)
-      .pipe(
-        map(val => { saveLocal(val); return val; }),
-        catchError(this.handleError('login', null)),
-      );
+    saveLocal(this.judges[2]);
+    return of(this.judges[2]);
+    // return this.http.get<Person>(url, options)
+    //   .pipe(
+    //     map(val => { saveLocal(val); return val; }),
+    //     catchError(this.handleError('login', null)),
+    //   );
   }
 
   logout() {
